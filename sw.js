879a1aa834543a6113424b12001bbe3731bd0de7
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-24 14:40:18 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "hilal-v3";
+
+self.addEventListener("install", e=>{
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(keys.map(k=>caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(fetch(e.request));
+});
